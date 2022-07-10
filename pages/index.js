e0dece027ba8ff4a707ef0bffc015bab46d1dd3a
Rahul Kukreja <rahul18254@iiitd.ac.in>
2022-07-11 03:28:17 +0530

    Changed title

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>Create Next App</title>
+                <title>Decentralised Raffle</title>
                 <meta name="description" content="Decentralised Raffle" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
